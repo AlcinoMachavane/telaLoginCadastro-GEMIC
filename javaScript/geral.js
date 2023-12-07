760281214ed7ea6b2fd15451df9178c9cde0058a
Machavane <150149062+Machavane@users.noreply.github.com>
2023-12-07 21:28:44 +0300

    V de teste

--- javaScript/geral.js
+++ javaScript/geral.js
@@ -1,3 +1,32 @@
-function bara_menu(){
+class NavBar {
+  constructor(menuAmburguer, menuBar, menu) {
+    this.menuAmburguer = document.querySelector(menuAmburguer);
+    this.menuBar = document.querySelector(menuBar);
+    this.menu = document.querySelectorAll(menu);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind();
+  }
 
+  
+
+  handleClick() {
+    console.log(this);
+    this.NavBar.classList.toggle(this.activeClass);
+  }
+
+  addClickEvent() {
+    this.menuAmburguer.addClickEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.menuAmburguer) {
+      this.addClickEvent();
     }
+    return this;
+  }
+
+const menuAmburguer = new this.menuAmburguer(
+  ".menuAmburguer",
+  ".menuBar",
+  ".menu");
+}
